24e991511410e1e4ddb43aa49bdfc81060575cfa
CodingalUsername <vannsh06@gmail.com>
2025-04-05 14:54:11 +0530

    init.commit

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 from config import HF_API_KEY
+import requests
+
+def classify_text(text):
+    API_URL="https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english"
+    headers={"Authorization":f"Bearer {HF_API_KEY}"}
+    payload={"inputs":text}
+
+    response=requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
+
+if __name__ =="__main__":
+    sample_text="I love using Hugging Face APIS!"
+    result=classify_text(sample_text)
+    print(result)
